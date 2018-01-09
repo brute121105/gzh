b48dad10cca84e649f2f9d96458e7e2fdd59372a
iex <752905288@qq.com>
2018-01-09 16:23:42 +0800

    =dsa

---
+++ src/main/java/weixin/gzh/server/conf/ValicodeCache.java
@@ -0,0 +1,7 @@
+package weixin.gzh.server.conf;
+
+public class ValicodeCache {
+	public static String phone="";
+	public static String valicode="";
+
+}
---
+++ src/main/java/weixin/gzh/server/model/Valicode.java
@@ -0,0 +1,6 @@
+package weixin.gzh.server.model;
+
+public class Valicode {
+	private String valicode;
+
+}
